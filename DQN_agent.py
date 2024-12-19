ef02d141e9911bb8e8dddcfef94f0a5d4e637b1d
Tanveen <tanveenbal@gmail.com>
2024-12-18 17:35:40 -0800

    Update DQN_agent.py

--- DQN_agent.py
+++ DQN_agent.py
@@ -1,15 +1,15 @@
 import os, sys
 
 ROOT = os.getcwd()
-sys.path.insert(1, f'{os.path.dirname(ROOT)}')
+sys.path.insert(1, f"{os.path.dirname(ROOT)}")
 
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 from collections import deque
+# from MinesweeperEnv import *
 from minesweeper_env import *
-# use my_tensorboard2.py if using tensorflow v2+, use my_tensorboard.py otherwise
-from my_tensorboard2 import *
+from tensorboard import *
 from DQN import *
 
 # Environment settings
@@ -29,15 +29,15 @@ EPSILON_DECAY = 0.99975
 EPSILON_MIN = 0.01
 
 # DQN settings
-CONV_UNITS = 64 # number of neurons in each conv layer
+CONV_UNITS = 128 # number of neurons in each conv layer
 DENSE_UNITS = 512 # number of neurons in fully connected dense layer
 UPDATE_TARGET_EVERY = 5
 
 # Default model name
-MODEL_NAME = f'conv{CONV_UNITS}x4_dense{DENSE_UNITS}x2_y{DISCOUNT}_minlr{LEARN_MIN}_8x8'
+MODEL_NAME = f"16x30_conv{CONV_UNITS}x4_dense{DENSE_UNITS}x2_y{DISCOUNT}_minlr{LEARN_MIN}"
 
 class DQNAgent(object):
-    def __init__(self, env, model_name=MODEL_NAME, conv_units=64, dense_units=256):
+    def __init__(self, env, model_name=MODEL_NAME, conv_units=CONV_UNITS, dense_units=DENSE_UNITS):
         self.env = env
 
         # Deep Q-learning Parameters
@@ -45,33 +45,41 @@ class DQNAgent(object):
         self.learn_rate = LEARN_RATE
         self.epsilon = epsilon
         self.model = create_dqn(
-            self.learn_rate, self.env.state_im.shape, self.env.ntiles, conv_units, dense_units)
+            self.learn_rate, (self.env.state_im.shape[0], self.env.state_im.shape[1], 1), self.env.ntiles, conv_units, dense_units)
 
         # target model - this is what we predict against every step
         self.target_model = create_dqn(
-            self.learn_rate, self.env.state_im.shape, self.env.ntiles, conv_units, dense_units)
+            self.learn_rate, (self.env.state_im.shape[0], self.env.state_im.shape[1], 1), self.env.ntiles, conv_units, dense_units)
         self.target_model.set_weights(self.model.get_weights())
 
         self.replay_memory = deque(maxlen=MEM_SIZE)
         self.target_update_counter = 0
 
         self.tensorboard = ModifiedTensorBoard(
-            log_dir=f'logs\\{model_name}', profile_batch=0)
+            log_dir=f"logs\\{model_name}", profile_batch=0)
 
     def get_action(self, state):
-        board = state.reshape(1, self.env.ntiles)
-        unsolved = [i for i, x in enumerate(board[0]) if x==-0.125]
-
+        unsolved = []
+        for row in range(self.env.nrows):
+            for col in range(self.env.ncols):
+                if state[row, col] == -0.125: 
+                    unsolved.append((row, col))
+        
         rand = np.random.random() # random value b/w 0 & 1
 
-        if rand < self.epsilon: # random move (explore)
-            move = np.random.choice(unsolved)
+        if rand < self.epsilon:
+            move = np.random.choice(range(len(unsolved)))
+            row, col = unsolved[move]
+            return row, col
         else:
+            # Make predictions using the model
             moves = self.model.predict(np.reshape(state, (1, self.env.nrows, self.env.ncols, 1)))
-            moves[board!=-0.125] = np.min(moves) # set already clicked tiles to min value
+            for row in range(self.env.nrows):
+                for col in range(self.env.ncols):
+                    if state[row, col] != -0.125: 
+                        moves[0, row * self.env.ncols + col] = -np.inf
             move = np.argmax(moves)
-
-        return move
+            return np.unravel_index(move, (self.env.nrows, self.env.ncols))
 
     def update_replay_memory(self, transition):
         self.replay_memory.append(transition)
@@ -83,9 +91,11 @@ class DQNAgent(object):
         batch = random.sample(self.replay_memory, BATCH_SIZE)
 
         current_states = np.array([transition[0] for transition in batch])
+        current_states = np.reshape(current_states, (BATCH_SIZE, self.env.nrows, self.env.ncols, 1))
         current_qs_list = self.model.predict(current_states)
 
         new_current_states = np.array([transition[3] for transition in batch])
+        new_current_states = np.reshape(new_current_states, (BATCH_SIZE, self.env.nrows, self.env.ncols, 1))
         future_qs_list = self.target_model.predict(new_current_states)
 
         X,y = [], []
@@ -98,7 +108,7 @@ class DQNAgent(object):
                 new_q = reward
 
             current_qs = current_qs_list[i]
-            current_qs[action] = new_q
+            current_qs[action[0] * self.env.ncols + action[1]] = new_q
 
             X.append(current_state)
             y.append(current_qs)
