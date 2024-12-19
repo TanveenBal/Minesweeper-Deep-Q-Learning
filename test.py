c82b46863d17901cfa0322b3df0a03611c52df49
TanveenBal <tanveenbal@gmail.com>
2024-12-18 19:42:08 -0800

    Updated test.py

--- test.py
+++ test.py
@@ -5,33 +5,36 @@ from keras.losses import MeanSquaredError
 from MinesweeperAgentWeb import *
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Play Minesweeper online using a DQN')
-    parser.add_argument('--model', type=str, default='conv64x4_dense512x2_y0.1_minlr0.001_8x8',
-                        help='name of model')
-    parser.add_argument('--episodes', type=int, default=100,
-                        help='Number of episodes to play')
+    parser = argparse.ArgumentParser(description="Play Minesweeper online using a DQN")
+    parser.add_argument("--model", type=str, default="16x16_conv128x4_dense512x2_y0.1_minlr0.001",
+                        help="name of model")
+    parser.add_argument("--episodes", type=int, default=10,
+                        help="Number of episodes to play")
 
     return parser.parse_args()
 
 params = parse_args()
 
-my_model = load_model(f'models/{params.model}.keras')
+my_model = load_model(f"models/{params.model}.keras")
 
 def main():
     pg.FAILSAFE = True
     agent = MinesweeperAgentWeb(my_model)
 
-    for _ in tqdm(range(1, params.episodes+1)):
+    for _ in tqdm(range(params.episodes)):
         agent.reset()
         agent.init_board()
 
         done = False
         while not done:
-            try:
+            # Try to evaluate the board first.
+            performed_action = agent.evaluate_board()
+
+            # If no moves from `evaluate_board`, fallback to model-based action.
+            if not performed_action:
                 action = agent.get_action()
-                state, done = agent.step(action)
-            except ValueError:
-                break
+                agent.step(action, uncover=True)
+            done = agent.done
 
 if __name__ == "__main__":
     main()
